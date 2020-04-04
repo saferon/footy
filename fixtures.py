8a97d62dca1381bfae11d06234e090049616e220
Saferon <cameronshandlite@gmail.com>
2020-04-04 17:31:55 +0100

    input update

--- fixtures.py
+++ fixtures.py
@@ -1,81 +1,123 @@
 import random
 import json
+import operator
 with open('scorekeeping.json', 'r') as scores:
     score_dict = json.load(scores)
 with open('guesses.json', 'r') as guesses:
     guesses_dict = json.load(guesses)
-
+with open('weekly.json', 'r') as weekly:
+    weekly_dict = json.load(weekly)
 # christie, neil, rambo, keith, hindu, dad
 # +2 points for result and score
 # +1 point for score
 
-teams = [
-    "Rangers", "Celtic", "Real Madrid", "Barcelona", "Man United", "Man City",
-    "Aberdeen", "Cove", "Peterhead", "Alloa", "Montrose", "Arbroath", "Liverpool", "Everton",
-    "Hearts", "Hibbs", "Accrington Stanley", "Forrest Green", "Leeds", "Notts", "Motherwell", "Airdrie", 
-    "Keith", "Devronvale", "West Ham", "Crystal Palace","West Brom", "Aston Villa"
-        ]
 
 def play_game(team1, team2, game):
-    players = ["christie", "mupps", "rambo", "keith", "hindu", "gav"]
-    roll1 = random.randint(0,4)
-    roll2 = random.randint(0,4)
+    """runs game"""
+    players = ["keith", "hindu", "rambo", "mupps", "christie", "gav"]
+    roll1 = random.randint(0,3)
+    roll2 = random.randint(0,3)
+    print("--------GAME" + " " +str(game + 1) + "--------")
     if roll1 == roll2:
         for people in players:
             guess = guesses_dict[people][game]
             if guess[0] == guess[1]:
                 score_dict[people] += 1
+                weekly_dict[people] += 1
                 if guess[0] == roll1 and guess[1] == roll2:
                     score_dict[people] += 2
+                    weekly_dict[people] += 2
             with open('scorekeeping.json', 'w') as dumping:
                 json.dump(score_dict, dumping)
-        print("It's a draw")
+            with open('weekly.json', 'w') as weeklydump:
+                json.dump(weekly_dict, weeklydump)
+            
+        # print("It's a draw")
     elif roll1 > roll2:
         for people in players:
             guess = guesses_dict[people][game]
             if guess[0] > guess[1]:
                 score_dict[people] += 1
+                weekly_dict[people] += 1
                 if guess[0] == roll1 and guess[1] == roll2:
                     score_dict[people] += 2
+                    weekly_dict[people] += 2
             with open('scorekeeping.json', 'w') as dumping:
                 json.dump(score_dict, dumping)
-        print(team1 + " is the winner")
+            with open('weekly.json', 'w') as weeklydump:
+                json.dump(weekly_dict, weeklydump)
+        # print(team1 + " is the winner")
     else:
         for people in players:
             guess = guesses_dict[people][game]
             if guess[0] < guess[1]:
                 score_dict[people] += 1
+                weekly_dict[people] += 1
                 if guess[0] == roll1 and guess[1] == roll2:
                     score_dict[people] += 2
+                    weekly_dict[people] += 1
             with open('scorekeeping.json', 'w') as dumping:
                 json.dump(score_dict, dumping)
-        print(team2 + " is the winner")
-    print(str(team1) + " " + str(roll1) + " - " + str(roll2)+ " " + str(team2))
+            with open('weekly.json', 'w') as weeklydump:
+                json.dump(weekly_dict, weeklydump)
+        # print(team2 + " is the winner")
+    print(str(team1) + " - " + str(roll1))
+    print(str(team2) + " - " + str(roll2))
+    print("-----------------------")
+
+
+def weeklyboard():
+    """prints out leaderboard for this week"""
+    week = sorted(weekly_dict.items(), key=operator.itemgetter(1), reverse=True)
+    print(" ")
+    print("---THIS WEEKS SCORES---")
+    # print(leaders)
+    for player in week:
+        print(str(player[0]).title() + " - " +str(player[1]))
+    print("-----------------------")
+
+
+def reset_weekly():
+    players = ["keith", "hindu", "rambo", "mupps", "christie", "gav"]
+    for people in players:
+        weekly_dict[people] = 0
+        with open('weekly.json', 'w') as weeklydump:
+                json.dump(weekly_dict, weeklydump)
 
 
 def leaderboard():
-    players = ["christie", "mupps", "rambo", "keith", "hindu", "gav"]
-    for player in players:
-        print(player + " - " + str(score_dict[player]))
+    leaders = sorted(score_dict.items(), key=operator.itemgetter(1), reverse=True)
+    print(" ")
+    print("------LEADERBOARD------")
+    # print(leaders)
+    for player in leaders:
+        print(str(player[0]).title() + " - " +str(player[1]))
+    print("-----------------------")
 
 
 ##########  format, HOME TEAM, AWAY TEAM ##########
-play_game("Rangers", "Celtic", 0)  # 1
-play_game("Real Madrid", "Barcelona", 1)  # 2
-play_game("Man United", "Man City", 2)  # 3
-play_game("Aberdeen", "Cove", 3)  # 4
-play_game("Peterhead", "Alloa", 4)  # 5
-play_game("Montrose", "Arbroath", 5)  # 6
-play_game("Liverpool", "Everton", 6)  # 7
-play_game("Hearts", "Hibbs", 7)  # 8
-play_game("Accrington Stanley", "Forrest Green", 8)  # 9
-play_game("Leeds", "Notts", 9)  # 10
-play_game("Motherwell", "Airdrie", 10)  # 11
-play_game("Keith", "Devronvale", 11)  # 12
-play_game("West Ham", "Crystal Palace", 12)  # 13
-play_game("West Brom", "Aston Villa", 13)  # 14
+print("-----------------------")
+play_game("Aberdeen", "Hearts", 0)  # 1 
+play_game("Kilmarnock", "Livingston", 1)  # 2
+play_game("Rangers", "Motherwell", 2)  # 3
+play_game("Ross County", "St Mirren", 3)  # 4 
+play_game("St John", "Hamilton", 4)  # 5
+play_game("Hibs", "Celtic", 5)  # 6
+play_game("Villa", "Wolves", 6)  # 7 
+play_game("Arsenal", "Norwich", 7)  # 8
+play_game("Bourmouth", "Newcastle", 8)  # 9
+play_game("Brighton", "Man United", 9)  # 10 
+play_game("Palace", "Burnley", 10)  # 11
+play_game("Watford", "Southham", 11)  # 12
+play_game("Sheffield Utd", "Spurs", 12)  # 13 
+play_game("West Ham", "Chelsea", 13)  # 14
+play_game("Man City", "Liverpool", 14)  # 15
+play_game("Everton", "Leicester", 15)  # 16 
 ###################################################
+weeklyboard()
 leaderboard()
+reset_weekly()
+
 # Week 1
 
 # Rangers - 1
@@ -135,3 +177,76 @@ leaderboard()
 # It's a draw
 # --------------------------------
 # {"gav": 5, "keith": 4, "christie": 3, "hindu": 7, "rambo": 2, "neil": 4}
+
+
+
+
+# midweek scores
+# christie - 0 #     (OVERALL = 3)
+# rambo - 0 #        (OVERALL = 2)
+# gav - 1 #          (OVERALL = 6)
+# keith - 5 #        (OVERALL = 9)
+# mupps - 4 #        (OVERALL = 8)
+# hindu - 0 #        (OVERALL = 7)
+# christie - 0 #     (OVERALL = 3)
+
+
+
+
+
+
+#
+#It's a draw
+#Ajax 0 - 0 Real Madrid
+#Celtic is the winner
+#Celtic 4 - 2 Athletico Madrid
+#It's a draw
+#Rangers 0 - 0 Hamburg
+#christie - 3
+##############################3
+# rambo
+#It's a draw
+#Aberdeen 4 - 4 Liverpool
+#Chelsea is the winner
+#Barcelona 1 - 3 Chelsea
+#Seville is the winner
+#Man City 0 - 3 Seville
+#rambo - 2
+#
+# hindu
+#Dortmund is the winner
+#Dortmund 3 - 1 Leverkusen
+#Juventeus is the winner
+#Juventeus 4 - 0 Lazio
+#It's a draw
+#Wolves 0 - 0 Galatasary
+#hindu - 7
+#
+# gav
+#It's a draw
+#Fingeroula 3 - 3 Espanolia
+#It's a draw
+#Brussia Munch 3 - 3 Elrick
+#It's a draw
+#Arsenal 3 - 3 Tottenham
+#gav - 6
+#
+# mupps
+#Leicester is the winner
+#Leicester 3 - 1 Porto
+#Marseille is the winner
+#Marseille 4 - 1 Watford
+#Newcastle is the winner
+#Newcastle 3 - 2 Nantes
+#mupps - 8
+#
+# keith
+#
+#Roma is the winner
+#Roma 3 - 1 Wolfsburg
+#Inter is the winner
+#Getaffe 1 - 2 Inter
+#Shakatier is the winner
+#Shakatier 3 - 1 Olyimpiacos
+#keith - 9
+
---
+++ take_guesses.py
@@ -0,0 +1,70 @@
+import json
+with open('guesses.json', 'r') as guesses:
+    guesses_dict = json.load(guesses)
+
+christie = input("input christies guesses: ")
+gav = input("input gavs guesses: ")
+hindu = input("input hindus guesses: ")
+keith = input("input keiths guesses: ")
+mupps = input("input mupps guesses: ")
+rambo = input("input rambos guesses: ")
+christie_guesses = []
+gav_guesses = []
+hindu_guesses = []
+keith_guesses = []
+mupps_guesses = []
+rambo_guesses = []
+valid_inputs = ["0","1","2","3","4","5","6","7","8","9"]
+for letters in christie:
+    if letters in valid_inputs:
+        christie_guesses.append(int(letters))
+for letters in gav:
+    if letters in valid_inputs:
+        gav_guesses.append(int(letters))
+for letters in hindu:
+    if letters in valid_inputs:
+        hindu_guesses.append(int(letters))
+for letters in keith:
+    if letters in valid_inputs:
+        keith_guesses.append(int(letters))
+for letters in mupps:
+    if letters in valid_inputs:
+        mupps_guesses.append(int(letters))
+for letters in rambo:
+    if letters in valid_inputs:
+        rambo_guesses.append(int(letters))
+
+
+def sort_lists_into_lists(fixme, person):
+    guesses_dict[person] = []
+    small_list = []
+    while len(fixme) > 0:
+        small_list.append(fixme[0])
+        fixme.pop(0)
+        small_list.append(fixme[0])
+        # print(small_list)
+        fixme.pop(0)
+        guesses_dict[person].append(small_list)
+        small_list = []
+    with open('guesses.json', 'w') as dumping:
+        json.dump(guesses_dict, dumping)
+
+
+def print_out_guesses():
+    players = ["christie", "gav", "hindu", "keith", "mupps", "rambo"]
+    for people in players:
+        dictionary = guesses_dict[people]
+        print(people + ": " + str(dictionary))
+
+
+#######################
+sort_lists_into_lists(christie_guesses, "christie")
+sort_lists_into_lists(gav_guesses, "gav")
+sort_lists_into_lists(hindu_guesses, "hindu")
+sort_lists_into_lists(keith_guesses, "keith")
+sort_lists_into_lists(mupps_guesses, "mupps")
+sort_lists_into_lists(rambo_guesses, "rambo")
+print("Guesses taken and sorted.")
+print_out_guesses()
+#######################
+
